07eec63bfc2e399402a5ddb5f1aea3dc94594979
imthaghost <imthaghost@protonmail.com>
2020-02-19 00:31:39 -0800

    [update] html render functinoality

---
+++ html/render.go
@@ -0,0 +1,24 @@
+package html
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo"
+)
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
